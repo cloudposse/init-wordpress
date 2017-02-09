12280d74297bf60558d3e7e0af1ed7b546f3825c
Igor Rodionov <goruha@gmail.com>
2017-02-09 15:27:05 +0600

    Added vars for init script

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -2,12 +2,16 @@
 SOURCE=${SOURCE:-""}
 DESTINATION=${DESTINATION:-""}
 DB=${DB:-""}
+
+echo "Start initialization..."
 cp -R  /source/code/$SOURCE/* /destination/code/$DESTINATION
-
+echo "Source code copied"
 if [[ -n "$DB" ]]; then
+  echo "Fetching database..."
   curl $DB > /tmp/db.gz
+  echo "Database fetched"
 fi
-
+echo "Sleeping...."
 sleep 5h
 
 
