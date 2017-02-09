abacea820e6c4f5ce84e9856c86271440af44867
Igor Rodionov <goruha@gmail.com>
2017-02-09 15:11:31 +0600

    Added vars for init script

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 SOURCE=${SOURCE:-""}
 DESTINATION=${DESTINATION:-""}
-
+DB=${DB:-""}
 cp -R  /source/code/$SOURCE/* /destination/code/$DESTINATION
+
+if [[ -n "$DB" ]]; then
+  curl $DB > /tmp/db.gz
+fi
+
+sleep 5h
 
 
 
