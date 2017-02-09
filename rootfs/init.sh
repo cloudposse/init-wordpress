50635327cb0538459f279d1acaa814ebf4110fae
Igor Rodionov <goruha@gmail.com>
2017-02-10 05:19:55 +0600

    Revert env vars

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -3,33 +3,30 @@
 GIT_REPO=${GIT_REPO:-""}
 GIT_BRANCH=${GIT_BRANCH:-"master"}
 
-SOURCE=${SOURCE:-""}
 DESTINATION=${DESTINATION:-""}
 DB=${DB:-""}
 
-mkdir -p /source/code
-mkdir -p /destination/code
+mkdir -p /destination/code/$DESTINATION
 
 if [[ -z "$GIT_REPO" ]]; then
   echo "GIT_REPO is not defined"
   exit 1
 fi
 
-cd /source/code/
-git clone --depth 1 $GIT_REPO
-cd /source/code/$SOURCE
-git checkout $GIT_BRANCH
-rm -rf /source/code/$SOURCE/.git
-
 echo "Start initialization..."
-cp -R  /source/code/$SOURCE/* /destination/code/$DESTINATION
+
+cd /destination/code/$DESTINATION
+git clone --depth 1 $GIT_REPO ./
+git checkout $GIT_BRANCH
+rm -rf /destination/code/$DESTINATION/.git
+
 echo "Source code copied"
 if [[ -n "$DB" ]]; then
   echo "Fetching database..."
   curl $DB > /tmp/db.gz
   echo "Database fetched"
   echo "Importing database..."
-  gzip -d < /tmp/db.gz | wp --path=/destination/code --allow-root db import -
+  gzip -d < /tmp/db.gz | wp --path=/destination/code/$DESTINATION --allow-root db import -
   echo "Done"
 fi
 
