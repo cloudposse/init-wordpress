136f8c794d79b60f6cec90b196bb828e1dfaee02
Igor Rodionov <goruha@gmail.com>
2017-02-10 03:52:57 +0600

    Fix git clone

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,9 @@ FROM soifou/wpcli-alpine
 
 ADD rootfs /
 
+RUN set -ex \
+      && apk update \
+      && apk add \
+        git
 
 ENTRYPOINT ["/init.sh"]
--- rootfs/init.sh
+++ rootfs/init.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
+
+GIT_REPO=${GIT_REPO:-""}
+GIT_BRANCH=${GIT_BRANCH:-"master"}
+
 SOURCE=${SOURCE:-""}
 DESTINATION=${DESTINATION:-""}
 DB=${DB:-""}
+
+mkdir -p /source/code
+mkdir -p /destination/code
+
+if [[ -z "$GIT_REPO" ]]; then
+  echo "GIT_REPO is not defined"
+  exit 1
+fi
+
+cd /source/code/
+git clone --depth 1 $GIT_REPO
+git checkout $GIT_BRANCH
+rm -rf /source/code/$SOURCE/.git
 
 echo "Start initialization..."
 cp -R  /source/code/$SOURCE/* /destination/code/$DESTINATION
