40dd7596a7d85f27c3eaa037461471b29d5530b3
Erik Osterman <e@osterman.com>
2017-02-11 11:04:28 -0800

    obfuscate output

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -2,16 +2,16 @@
 
 set -o pipefail
 
-GIT_REPO=${GIT_REPO:-""}
+GIT_REPO_URL=${GIT_REPO_URL:-""}
 GIT_BRANCH=${GIT_BRANCH:-"master"}
 
 DESTINATION=${DESTINATION:-"/destination"}
 DB_URL=${DB:-""}
 DB_DUMP=${DB_DUMP:-/tmp/db.sql.gz}
 
-if [ -n "$GIT_REPO" ]; then
-  echo "Cloning ${GIT_REPO}#${GIT_BRANCH}..."
-  git clone --depth 1 -b "${GIT_BRANCH}" "${GIT_REPO}" "${DESTINATION}"
+if [ -n "$GIT_REPO_URL" ]; then
+  echo "Cloning ${GIT_REPO_URL}#${GIT_BRANCH}..." | sed -E 's://[^@]+@://xxxxxxxx@:g'
+  git clone --depth 1 -b "${GIT_BRANCH}" "${GIT_REPO_URL}" "${DESTINATION}"
   if [ $? -ne 0 ]; then
     echo "Failed"
     exit 1
@@ -22,7 +22,7 @@ if [ -n "$GIT_REPO" ]; then
 fi
 
 if [ -n "${DB_URL}" ]; then
-  echo "Fetching database backup from ${DB_URL}..."
+  echo "Fetching database backup from ${DB_URL}..." | sed -E 's://[^@]+@://xxxxxxxx@:g'
   curl --fail -s "${DB_URL}" > "${DB_DUMP}"
   if [ $? -ne 0 ]; then
     echo "Failed to download database to ${DB_DUMP}"
